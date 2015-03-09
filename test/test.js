4b976fca4dc29e4d2b4efb9beb5b40c8e2b59793
unknown <ptgrogan@ptgrogan-dell.mit.edu>
2015-03-09 17:00:17 -0500

    bug fixing; implementing other features

--- test/test.js
+++ test/test.js
@@ -21,11 +21,36 @@ requirejs.config({
   nodeRequire: require
 });
 
-requirejs(['mas','fss-ofs'], function(mas,fss) {
-	console.log('this is a test');
+requirejs(['mas','fss-ofs','game'], function(mas,fss,Game) {
+	var game = new Game();
+	var context = game.buildContext(0);
+	var sim = new mas.sim.Simulator({
+		entities: [context],
+		initTime: 0,
+		timeStep: 1,
+		maxTime: 5
+	});
+	sim.init();
 	
+	console.log(context.locations);
 	
+	console.log(context.federations[0].federates[0]);
 	
+	var gs = new fss.GroundStation({name: "X", cost: 500, maxSize: 4});
+	var sc = new fss.Spacecraft({name: "A", cost: 200, maxSize: 2});
+	context.federations[0].federates[0].design(gs);
+	var b = context.federations[0].federates[0].commission(gs, 
+			context.locations[0], context);
+
+	console.log(context.federations[0].federates[0]);
 	
+	context.federations[0].federates[0].design(sc);
+	context.federations[0].federates[0].commission(sc, 
+			context.locations[1], context);
+			
+	console.log(context.federations[0].federates[0]);
 	
+	sim.advance();
+	
+	console.log(context.federations[0].federates[0]);
 });
