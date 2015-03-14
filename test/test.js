cfcb54912ee42ce94cbd75333aca55d2555e6feb
unknown <ptgrogan@ptgrogan-dell.mit.edu>
2015-03-13 19:23:48 -0500

    more updates

--- test/test.js
+++ test/test.js
@@ -28,29 +28,13 @@ requirejs(['underscore','mas','fss-ofs','game'], function(_,mas,fss,Game) {
 		entities: [context],
 		initTime: 0,
 		timeStep: 1,
-		maxTime: 5
+		maxTime: 10
 	});
 	sim.init();
 	
-	/*
-	var gs = new fss.GroundStation({
-		id: "X", 
-		cost: 500, 
-		maxSize: 4, 
-		subsystems: []
-	});
-	var sc = new fss.Spacecraft({
-		id: "A", 
-		cost: 200, 
-		maxSize: 2, 
-		subsystems: []
-	});
-	*/
-	
-	
 	sim.on("init", function() {
 		var gs = new fss.GroundStation(_.findWhere(game.systemTypes, {type: "Ground Station"}));
-		gs.subsystems.push(new fss.Subsystem(_.findWhere(game.subsystemTypes, {type: "SGL(p)"})));
+		gs.subsystems.push(new fss.SpaceGroundLink(_.findWhere(game.subsystemTypes, {type: "SGL(p)"})));
 		
 		var federate = context.federations[0].federates[0];
 		
@@ -58,8 +42,8 @@ requirejs(['underscore','mas','fss-ofs','game'], function(_,mas,fss,Game) {
 		federate.commission(gs, context.locations[0], context);
 		
 		var sc = new fss.Spacecraft(_.findWhere(game.systemTypes, {type: "Small Sat"}));
-		sc.subsystems.push(new fss.Subsystem(_.findWhere(game.subsystemTypes, {type: "SAR"})));
-		sc.subsystems.push(new fss.Subsystem(_.findWhere(game.subsystemTypes, {type: "SGL(p)"})));
+		sc.subsystems.push(new fss.Sensor(_.findWhere(game.subsystemTypes, {type: "SAR"})));
+		sc.subsystems.push(new fss.SpaceGroundLink(_.findWhere(game.subsystemTypes, {type: "SGL(p)"})));
 		
 		federate.design(sc);
 		federate.commission(sc, context.locations[1], context);
@@ -69,22 +53,22 @@ requirejs(['underscore','mas','fss-ofs','game'], function(_,mas,fss,Game) {
 		console.log('Turn ' + time);
 		console.log('Cash ' + context.federations[0].federates[0].cash);
 		
-		var federate = context.federations[0].federates[0];
-		var event = _.findWhere(context.currentEvents, {sector: _.findWhere(context.locations, {id: federate.systems[1].location}).sector});
-		
-		console.log(event);
-
-		if(federate.systems[1].canSense(event, context)) {
-			if(contract = federate.contract(event, context)) {
-				federate.sense(contract, federate.systems[1], context);
-			}
-		}
-		console.log(federate.systems[1].subsystems[0].contents);
+		_.each(context.federations, function(federation) {
+			_.each(federation.federates, function(federate) {
+				federate.autoContractAndSense(context);
+				federate.autoDownlink(context);
+				federate.autoContractAndSense(context);
+			});
+		});
 	});
 	
 	sim.on("complete", function() {
-		context.federations[0].federates[0].liquidate(context);
-		console.log(context.federations[0].federates[0]);
+		_.each(context.federations, function(federation) {
+			_.each(federation.federates, function(federate) {
+				federate.liquidate(context);
+				console.log(federate);
+			}, this);
+		}, this);
 	});
 	
 	sim.execute();
